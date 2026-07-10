2aaa2236b6edf88a076a269c151851fd3a938059
bigmanjtech <bigmanj429@gmail.com>
2026-07-10 22:01:24 +0300

    Create exports.js

---
+++ tools/exports.js
@@ -0,0 +1,9 @@
+// Export required modules or functions
+const tools = {
+    api: require("./api.js"),
+    cmd: require("./cmd.js"),
+    list: require("./list.js"),
+    msg: require("./msg.js")
+};
+
+module.exports = tools;
