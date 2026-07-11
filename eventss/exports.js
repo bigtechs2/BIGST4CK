dbf42816d35f30fdc96f57f2f5a21edfea0324da
bigmanjtech <bigmanj429@gmail.com>
2026-07-11 06:42:54 +0300

    Delete exports.js

--- eventss/exports.js
+++
@@ -1,8 +0,0 @@
-module.exports = (bot) => {
-    bot.ev.setMaxListeners(config.system.maxListeners); // Set max listeners for events
-
-    require("./ready.js")(bot);
-    require("./messages.js")(bot);
-    require("./welcome.js")(bot);
-    require("./call.js")(bot);
-}
