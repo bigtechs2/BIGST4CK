beec8c2830f5d020bff95c78eae38f7b69fea654
bigmanjtech <bigmanj429@gmail.com>
2026-08-14 22:23:07 +0300

    Create thankto.js

---
+++ commands/information/thankto.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "thankto",
+    aliases: ["thanksto"],
+    category: "information",
+    code: async (ctx) => {
+        await ctx.reply(
+            "› bigtechs2 (github.com/bigtechs2)\n" +
+            "› bigtechs1 (github.com/bigtechs1)\n" +
+            "› bigmanj tech ™ with ♥︎\n" +
+            "› And to everyone who has helped in the development of this bot. (Too many to list one by one)"
+        ); // If you don't remove this, thank you!
+    }
+};
